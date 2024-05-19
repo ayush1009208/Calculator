cc029ad064c4d5fb3a4e7a3d6164bc93d0040fa3
ayush1009208 <159469387+ayush1009208@users.noreply.github.com>
2024-05-19 23:26:43 +0530

    Ye le bsdk

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-document.getElementById("changeTextBtn").addEventListener("click", function() {
-    document.getElementById("example").textContent = "Text changed!";
-});
+function clearDisplay() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('result').value;
+    document.getElementById('result').value = display.slice(0, -1);
+}
+
+function appendToDisplay(value) {
+    document.getElementById('result').value += value;
+}
+
+function calculateResult() {
+    try {
+        let display = document.getElementById('result').value;
+        let result = eval(display);
+        document.getElementById('result').value = result;
+    } catch (e) {
+        document.getElementById('result').value = 'Error';
+    }
+}
